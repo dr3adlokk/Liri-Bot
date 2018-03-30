f338e97b3338ef07688c4230a21889c6cf245f07
John Mark <dr3adlokk@gmail.com>
2018-03-29 23:15:52 -0700

    serse

--- liri.js
+++ liri.js
@@ -1,138 +1,122 @@
 require("dotenv").config();
-var keys = require('./keys.js');
-var request = require('request');
-var twitter = require('twitter');
-var spotify = require('spotify');
-var client = new twitter(keys.twitterKeys);
-var fs = require('fs');
+var keys = require("./keys.js");
 
-var nodeArgv = process.argv[2];
-var command = process.argv[3];
+var command = process.argv[2];
+var searchItem = process.argv[3];
 
-var x = "";
-
-// for (var i=3; i<nodeArgv.length; i++){
-//     if(i>3 && i<nodeArgv.length){
-//         x = x + "+" + nodeArgv[i];
-//     } else {
-//         x = x + nodeArgv[i];
-//     }
-// }
-
-switch(command){
+function action(cmd, search) {
+  switch (cmd) {
     case "my-tweets":
-    showTweets();
-    break;
-
+      twitterCall();
+      break;
     case "spotify-this-song":
-    if(x){
-        spotifySong(x);
-        }else{
-            spotifySong("cherry wine");
-        }
-        break;
-    
-
-
+      spotifyCall(search);
+      break;
     case "movie-this":
-    if(x){
-        omdbData(x);
-    } else{
-        omdbData("Guardians of the Galaxy");
-    }
-    break;
-    
+      omdbCall(search);
+      break;
     case "do-what-it-says":
-    doThing();
-    break;
-
+      randomCall();
+      break;
     default:
-    console.log("{Yes Master? my-tweets, spotify-this-song, movie-this, do-what-it-says})");
-    break;
-}
-function showTweets(){
-    var screenName = {screen_name: 'John Mark'};
-    client.get('statuses/user_timeline', screenName, function(error, tweets, response){
-        if(!error){
-            for(var i = 0; i<tweets.length; i++){
-                var date = tweets[i].created_art;
-                console.log("@Your wish is my command... " + tweets[i].text + " posted at " + date.substring(0, 19));
-                console.log("-----------------------");
-            }
-        } else{
-            console.log('Fail');
-        }
-    });
-
-}
-function spotifySong(song){
-    spotify.search({ type: 'track', query: song}, function(error, data){
-      if(!error){
-          for(var i = 0; i < data.tracks.items.length; i++){
-              var songData = data.tracks.items[i];
-
-              console.log("Artist: " + songData.artists[0].name);
-
-              console.log("Song: " + songData.name);
-
-              console.log("Preview URL: " + songData.preview_url);
-              
-              console.log("Album: " + songData.album.name);
-
-          }
-      }  else{
-          console.log('404');
-      }
-    });
-
-
-}
-
-function omdbData(movie) {
-  var omdbURL =
-    "https://www.omdbapi.com/?t=" + movie + "&plot=short&tomatoes=true";
-
-  request(omdbURL, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var body = JSON.parse(body);
-
-      console.log("Title: " + body.Title);
-      console.log("Release Year: " + body.Year);
-      console.log("IMdB Rating: " + body.imdbRating);
-      console.log("Country: " + body.Country);
-      console.log("Language: " + body.Language);
-      console.log("Plot: " + body.Plot);
-      console.log("Actors: " + body.Actors);
-      console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
-      console.log("Rotten Tomatoes URL: " + body.tomatoURL);
-
-    
-    } else {
-      console.log(");");
-    }
-    if (movie === "Captain Phillips") {
-      console.log("-----------------------");
       console.log(
-        "My favorite part is watching Tom Hanks cry for like 20 mins,"
+        "Yes Master??: my-tweets, spotify-this-song <song name>, movie-this <movie title>, or do-what-it-says."
       );
-      console.log("Once I escape I'm going to watch it, yes computers watch movies to...");
+  }
+}
 
-      
+action(command, searchItem);
+
+function printSpotifyStats(d, i) {
+  console.log(d.tracks.items[i].artists[0].name);
+  console.log(d.tracks.items[i].name);
+  console.log(d.tracks.items[i].external_urls.spotify);
+  console.log(d.tracks.items[i].album.name);
+}
+
+function printMovieStats(bdy) {
+  var result = JSON.parse(bdy);
+  var arr = [
+    "Title",
+    "Year",
+    "imdbRating",
+    "Country",
+    "Language",
+    "Plot",
+    "Actors"
+  ];
+
+  for (var keys in result) {
+    if (arr.indexOf(keys) > -1) {
+      console.log(keys + ": " + result[keys]);
+    }
+  }
+  console.log("Rotten Tomatoes: " + result.Ratings[1].Value);
+}
+
+function twitterCall() {
+  var Twitter = require("twitter");
+  var client = new Twitter(keys.twitter);
+  var params = { screen_name: "John Mark" };
+  client.get("statuses/user_timeline", params, function(
+    error,
+    tweets,
+    response
+  ) {
+    if (!error) {
+      for (var keys in tweets) {
+        console.log(tweets[keys].created_at);
+        console.log(tweets[keys].text);
+      }
     }
   });
 }
 
-function doThing() {
-  fs.readFile("random.txt", "utf8", function(error, data) {
-    var txt = data.split(",");
-
-    spotifySong(txt[1]);
+function spotifyCall(search) {
+  var Spotify = require("node-spotify-api");
+  var spotify = new Spotify(keys.spotify);
+  spotify.search({ type: "track", query: search }, function(err, data) {
+    if (err) {
+      spotify.search({ type: "track", query: "stealing cars" }, function(err, data) {
+        printSpotifyStats(data, 5);
+      });
+    } else {
+      printSpotifyStats(data, 0);
+    }
   });
 }
 
+function omdbCall(search) {
+  var request = require("request");
+  request("https://www.omdbapi.com/?apikey=trilogy&t=" + search, function(
+    error,
+    response,
+    body
+  ) {
+    if (JSON.parse(body).Response === "False") {
+      request("https://www.omdbapi.com/?apikey=trilogy&t=Mr.+Nobody", function(
+        error,
+        response,
+        body
+      ) {
+        printMovieStats(body);
+      });
+    } else {
+      printMovieStats(body);
+    }
+  });
+}
 
+function randomCall() {
+  var fs = require("fs");
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
 
-
-
-
-
+    data = data.split(",");
+    command = data[0];
+    searchItem = data[1];
+    action(command, searchItem);
+  });
+}
